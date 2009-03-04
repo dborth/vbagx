169b1642600bff4bab293251ff59deb5168ca713
dborth <dborth@ab33defa-829a-11dd-99d6-6900107db72b>
2009-03-04 06:53:25 +0000

    Carl Kenner's VBA-M changes

--- trunk/source/vba/gba/GBAcpu.h
+++ trunk/source/vba/gba/GBAcpu.h
@@ -5,7 +5,8 @@ extern int armExecute();
 extern int thumbExecute();
 
 #ifdef __GNUC__
-# define INSN_REGPARM __attribute__((regparm(1)))
+# define INSN_REGPARM /*nothing*/
+  //# define INSN_REGPARM __attribute__((regparm(1)))
 # define LIKELY(x) __builtin_expect(!!(x),1)
 # define UNLIKELY(x) __builtin_expect(!!(x),0)
 #else
