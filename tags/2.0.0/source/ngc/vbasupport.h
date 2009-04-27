cfc65262d3497b7fcd55a248d296b54c6577cf6a
dborth <dborth@ab33defa-829a-11dd-99d6-6900107db72b>
2009-04-27 07:18:02 +0000

    [2.0.0 - April 27, 2009] * New GX-based menu, with a completely redesigned layout. Has Wiimote IR   support, sounds, graphics, animation effects, and more * Thanks to the3seashells for designing some top-notch artwork, to   Peter de Man for composing the music, and a special thanks to shagkur for   fixing libogc bugs that would have otherwise prevented the release * Onscreen keyboard for changing save/load folders and network settings * Menu configuration options (configurable exit button, wiimote orientation,   volumes) * New save manager, allowing multiple saves and save browsing. Shows   screenshots for Snapshot saves, and save dates/times * Added video shifting option * Added video mode selection (recommended to leave on Automatic) * USB Mouse support (buttons only) * Keyboard shift key bug fixed * Built-in 14 colour palettes for some monochrome gameboy games (Magnetic   Soccer, Malibu Beach Volleyball, Marble Madness, Metroid 2, Mortal Kombat,   Mortal Kombat II, Mortal Kombat 3, Mr. Do!) * Rumble works in GBC games designed for rumble cartridges but shipped   without rumble cartridges, such as Disney's Tarzan for GBC * Improved Mortal Kombat Wii Controls * Mortal Kombat games now have many extra characters to choose * Wii Controls for more Teenage Mutant Ninja Turtles games * Improved Lego Star Wars controls * Boktai menu now tells you when there can't be sun because it is night * Minor bug fixes

---
+++ tags/2.0.0/source/ngc/vbasupport.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+ * Visual Boy Advance GX
+ *
+ * Tantric September 2008
+ *
+ * vbasupport.h
+ *
+ * VBA support code
+ ***************************************************************************/
+
+#ifndef VBASUPPORT_H
+#define VBASUPPORT_H
+
+#include "System.h"
+
+extern struct EmulatedSystem emulator;
+extern u32 loadtimeradjust;
+extern int cartridgeType;
+extern int SunBars;
+extern u32 RomIdCode;
+extern bool TiltSideways;
+
+bool LoadVBAROM(int method);
+void InitialisePalette();
+bool LoadBatteryOrState(char * filepath, int method, int action, bool silent);
+bool LoadBatteryOrStateAuto(int method, int action, bool silent);
+bool SaveBatteryOrState(char * filepath, int method, int action, bool silent);
+bool SaveBatteryOrStateAuto(int method, int action, bool silent);
+
+#endif
