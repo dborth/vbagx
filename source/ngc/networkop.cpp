276e68e92f1bfa43c216110f5d3ab4ae0cdf6c45
dborth <dborth@ab33defa-829a-11dd-99d6-6900107db72b>
2010-03-15 07:46:11 +0000

    changes for multi-language support

--- source/ngc/networkop.cpp
+++ source/ngc/networkop.cpp
@@ -234,6 +234,7 @@ void CloseShare()
 	if(networkShareInit)
 		smbClose("smb");
 	networkShareInit = false;
+	isMounted[DEVICE_SMB] = false;
 }
 
 /****************************************************************************
@@ -272,15 +273,12 @@ ConnectShare (bool silent)
 		return false;
 	}
 
-	if(unmountRequired[DEVICE_SMB])
-		CloseShare();
-
 	if(!networkInit)
 		InitializeNetwork(silent);
 
 	if(!networkInit)
 		return false;
-
+	
 	while(retry)
 	{
 		if(!silent)
