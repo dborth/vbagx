0893c0857bb073f2ef6cb6514f46390f89cf62c4
dborth <dborth@ab33defa-829a-11dd-99d6-6900107db72b>
2010-05-19 21:52:59 +0000

    finalize 2.1.6

--- source/vbagx.h
+++ source/vbagx.h
@@ -16,7 +16,7 @@
 #include "utils/FreeTypeGX.h"
 
 #define APPNAME 		"Visual Boy Advance GX"
-#define APPVERSION 		"2.1.5"
+#define APPVERSION 		"2.1.6"
 #define APPFOLDER 		"vbagx"
 #define PREF_FILE_NAME 	"settings.xml"
 #define PAL_FILE_NAME 	"palettes.xml"
