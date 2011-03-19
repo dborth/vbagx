12efc3ebdbfa7df19c634c6d49ecac87ea58b9fa
dborth <dborth@ab33defa-829a-11dd-99d6-6900107db72b>
2011-03-19 05:35:43 +0000

    [2.2.3 - March 19, 2011]
    * Improved USB and controller compatibility (recompiled with latest libogc)
    * Enabled SMB on GameCube (thanks Extrems!)
    * Added Catalan translation
    * Translation updates

---
+++ tags/2.2.3/source/button_mapping.c
@@ -0,0 +1,116 @@
+/****************************************************************************
+ * Visual Boy Advance GX
+ *
+ * Tantric September 2008
+ *
+ * button_mapping.c
+ *
+ * Controller button mapping
+ ***************************************************************************/
+
+#include <gccore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ogcsys.h>
+#include <unistd.h>
+#include <wiiuse/wpad.h>
+
+#include "button_mapping.h"
+
+/****************************************************************************
+ * Controller Button Descriptions:
+ * used for identifying which buttons have been pressed when configuring
+ * and for displaying the name of said button
+ ***************************************************************************/
+
+CtrlrMap ctrlr_def[4] = {
+// Gamecube controller btn def
+{
+	{
+		{PAD_BUTTON_DOWN, "DOWN"},
+		{PAD_BUTTON_UP, "UP"},
+		{PAD_BUTTON_LEFT, "LEFT"},
+		{PAD_BUTTON_RIGHT, "RIGHT"},
+		{PAD_BUTTON_A, "A"},
+		{PAD_BUTTON_B, "B"},
+		{PAD_BUTTON_X, "X"},
+		{PAD_BUTTON_Y, "Y"},
+		{PAD_BUTTON_MENU, "START"},
+		{PAD_BUTTON_START, "START"},
+		{PAD_TRIGGER_L, "L"},
+		{PAD_TRIGGER_R, "R"},
+		{PAD_TRIGGER_Z, "Z"},
+		{0, ""},
+		{0, ""}
+	},
+	13,
+	CTRLR_GCPAD
+},
+// Wiimote btn def
+{
+	{
+		{WPAD_BUTTON_DOWN, "DOWN"},
+		{WPAD_BUTTON_UP, "UP"},
+		{WPAD_BUTTON_LEFT, "LEFT"},
+		{WPAD_BUTTON_RIGHT, "RIGHT"},
+		{WPAD_BUTTON_A, "A"},
+		{WPAD_BUTTON_B, "B"},
+		{WPAD_BUTTON_1, "1"},
+		{WPAD_BUTTON_2, "2"},
+		{WPAD_BUTTON_PLUS, "PLUS"},
+		{WPAD_BUTTON_MINUS, "MINUS"},
+		{WPAD_BUTTON_HOME, "HOME"},
+		{0, ""},
+		{0, ""},
+		{0, ""},
+		{0, ""}
+	},
+	11,
+	CTRLR_WIIMOTE
+},
+// Nunchuk btn def
+{
+	{
+		{WPAD_BUTTON_DOWN, "DOWN"},
+		{WPAD_BUTTON_UP, "UP"},
+		{WPAD_BUTTON_LEFT, "LEFT"},
+		{WPAD_BUTTON_RIGHT, "RIGHT"},
+		{WPAD_BUTTON_A, "A"},
+		{WPAD_BUTTON_B, "B"},
+		{WPAD_BUTTON_1, "1"},
+		{WPAD_BUTTON_2, "2"},
+		{WPAD_BUTTON_PLUS, "PLUS"},
+		{WPAD_BUTTON_MINUS, "MINUS"},
+		{WPAD_BUTTON_HOME, "HOME"},
+		{WPAD_NUNCHUK_BUTTON_Z, "Z"},
+		{WPAD_NUNCHUK_BUTTON_C, "C"},
+		{0, ""},
+		{0, ""}
+	},
+	13,
+	CTRLR_NUNCHUK
+},
+// Classic btn def
+{
+	{
+		{WPAD_CLASSIC_BUTTON_DOWN, "DOWN"},
+		{WPAD_CLASSIC_BUTTON_UP, "UP"},
+		{WPAD_CLASSIC_BUTTON_LEFT, "LEFT"},
+		{WPAD_CLASSIC_BUTTON_RIGHT, "RIGHT"},
+		{WPAD_CLASSIC_BUTTON_A, "A"},
+		{WPAD_CLASSIC_BUTTON_B, "B"},
+		{WPAD_CLASSIC_BUTTON_X, "X"},
+		{WPAD_CLASSIC_BUTTON_Y, "Y"},
+		{WPAD_CLASSIC_BUTTON_PLUS, "PLUS"},
+		{WPAD_CLASSIC_BUTTON_MINUS, "MINUS"},
+		{WPAD_CLASSIC_BUTTON_HOME, "HOME"},
+		{WPAD_CLASSIC_BUTTON_FULL_L, "L"},
+		{WPAD_CLASSIC_BUTTON_FULL_R, "R"},
+		{WPAD_CLASSIC_BUTTON_ZL, "ZL"},
+		{WPAD_CLASSIC_BUTTON_ZR, "ZR"}
+	},
+	15,
+	CTRLR_CLASSIC
+}
+};
---
+++ tags/2.2.3/source/networkop.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+ * Visual Boy Advance GX
+ *
+ * Tantric December 2008
+ *
+ * networkop.h
+ *
+ * Network and SMB support routines
+ ****************************************************************************/
+
+#ifndef _NETWORKOP_H_
+#define _NETWORKOP_H_
+
+void UpdateCheck();
+bool DownloadUpdate();
+void StartNetworkThread();
+bool InitializeNetwork(bool silent);
+bool ConnectShare (bool silent);
+void CloseShare();
+
+extern bool updateFound;
+
+#endif
