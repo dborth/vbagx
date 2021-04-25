f3057ca59b53cb26d9f2cce62d6aba216c39c883
Juan Carlos Ruvalcaba <askot.altair@gmail.com>
2021-04-25 15:11:31 -0700

    Refactor -again- change theme.

--- src/js/stores/theme.js
+++ src/js/stores/theme.js
@@ -1,4 +1,6 @@
 import { createStore } from 'framework7';
+import $ from 'dom7';
+import * as localForage from "localforage";
 
 const theme = createStore({
   state: {
@@ -7,6 +9,21 @@ const theme = createStore({
   actions: {
     changeTheme({ state }, name) {
       state.name = name;
+    },
+    async checkTheme({ state, dispatch }, { selector }){
+      let theme = await localForage.getItem('theme');
+      if (theme != 'theme-dark'){ $(selector).removeClass('theme-dark'); }
+      dispatch('changeTheme', theme);
+    },
+    async initTheme({state, dispatch}, { selector }){
+      let items = await localForage.length();
+      if (items > 0){
+        await dispatch('checkTheme', { selector });
+      }
+    },
+    async setTheme({ state, dispatch }, name){
+      await localForage.setItem('theme', name);
+      dispatch('changeTheme', name);
     },
   },
   getters: {
