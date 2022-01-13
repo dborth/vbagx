603f018a336d3c693acb81876ed98199c14ae9e8
Daryl Borth <dborth@gmail.com>
2022-01-13 14:37:31 -0700

    support forwarders that pass in sd1:/

--- source/fileop.cpp
+++ source/fileop.cpp
@@ -459,8 +459,8 @@ void CreateAppPath(char * origpath)
 
 	int pos = 0;
 
-	// replace fat:/ with sd:/
-	if(strncmp(path, "fat:/", 5) == 0)
+	// replace fat:/ or sd1:/ with sd:/
+	if(strncmp(path, "fat:/", 5) == 0 || strncmp(path, "sd1:/", 5) == 0)
 	{
 		pos++;
 		path[1] = 's';
