180364ecde142c766ee515c7fa8556023b0314e8
Daryl Borth <dborth@gmail.com>
2026-09-02 16:44:45 -0600

    drop in new device management code

--- source/vbagx.h
+++ source/vbagx.h
@@ -14,6 +14,7 @@
 #include <sys/param.h>
 
 #include "drivers/InputDriver.h"
+#include "drivers/ogc/OgcDeviceTypes.h"
 #include "filelist.h"
 
 #define APPNAME 		"Visual Boy Advance GX"
@@ -25,23 +26,6 @@
 #define NOTSILENT 0
 #define SILENT 1
 #define MAX_GBA_ROM_SIZE (1024*1024*32)
-
-const char pathPrefix[10][11] =
-{ "", "sd:/", "usb:/", "dvd:/", "smb:/", "carda:/", "cardb:/", "port2:/", "gcloader:/" };
-
-enum 
-{
-	DEVICE_AUTO = 0,
-	DEVICE_SD,
-	DEVICE_USB,
-	DEVICE_DVD,
-	DEVICE_SMB,
-	DEVICE_SD_SLOTA,
-	DEVICE_SD_SLOTB,
-	DEVICE_SD_PORT2,
-	DEVICE_SD_GCLOADER,
-	DEVICE_LENGTH
-};
 
 enum {
     SAVEFOLDER_SAVES = 0,
