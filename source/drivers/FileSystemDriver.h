a48e750da9dd76ee99de972a92f774d258aad956
Daryl Borth <dborth@gmail.com>
2026-09-15 13:29:42 -0600

    async image loading in filebrowser

--- source/drivers/FileSystemDriver.h
+++ source/drivers/FileSystemDriver.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 
 #include "SmbDriver.h"
+#include "Mutex.h"
 
 #define MAX_STORAGE_DEVICES 16
 
@@ -59,6 +60,9 @@ enum class MountResult
 	MountFailed     //!< present, but couldn't be mounted (eg. unrecognized format)
 };
 
+//!Storage device enumeration/mount/poll backend for the SD/USB/DVD file
+//!browser. Exactly one driver implements this and assigns the single
+//!global Platform instance.
 class FileSystemDriver
 {
 	public:
@@ -136,6 +140,14 @@ class FileSystemDriver
 		virtual const int * getValidSaveDevices(int & outCount) const = 0;
 
 		virtual SmbDriver * getSmb() = 0;
+
+		//! Serializes raw devoptab I/O (open/read/write/readdir/stat) issued
+		//! by more than one thread against a mounted volume.
+		static Mutex & getIoLock()
+		{
+			static Mutex lock;
+			return lock;
+		}
 };
 
 //! Convenience for "try these devices in priority order, use whichever one
