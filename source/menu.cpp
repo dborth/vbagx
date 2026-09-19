30b1bb77884901c4b65f69d88ef1a35727b3bcd6
Daryl Borth <dborth@gmail.com>
2026-09-19 10:29:11 -0600

    fix flash switching between menus

--- source/menu.cpp
+++ source/menu.cpp
@@ -532,10 +532,12 @@ static void ServicePendingWindowPromptRequest()
  * must stop immediately and return control up the call stack
  ***************************************************************************/
 
+// Set once the OS has asked the app to quit and the fade-out has played
+static bool guiExiting = false;
+
 static bool UpdateGui()
 {
-	static bool exiting = false;
-	if(exiting)
+	if(guiExiting)
 		return false;
 
 	ProcessInputData();
@@ -554,7 +556,7 @@ static bool UpdateGui()
 			platform->getVideo()->getImageRenderer()->drawRectangle(0,0,platform->getVideo()->getScreenWidth(),platform->getVideo()->getScreenHeight(),(PixelColor){0, 0, 0, (uint8_t)a});
 			platform->getVideo()->renderMenu();
 		}
-		exiting = true;
+		guiExiting = true;
 		return false;
 	}
 
@@ -5586,7 +5588,7 @@ void MainMenu (int selection)
 		}
 		lastMenu = currentMenu;
 
-		if(!UpdateGui())
+		if(guiExiting)
 			break;
 	}
 
